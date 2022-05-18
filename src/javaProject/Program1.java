d2bb9c650f85cab21709d1a1f43443915f6c625a
Sanchit kumar <sanchit77kumar@gmail.com>
2022-05-18 13:52:35 +0530

    Program1_PracticeInterview_Sanchit

--- src/javaProject/Program1.java
+++ src/javaProject/Program1.java
@@ -3,6 +3,6 @@ package javaProject;
 public class Program1 {
 
 	public static void main(String[] args) {
-		System.out.println("PracticeInterview-Project Upload");
+		System.out.println("PracticeInterview-Repo Upload");
 	}
 }
